bff59f2237873346388559c3c44d3652b5263274
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-05-02 15:10:02 +0900

    Initial commit

---
+++ run.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+killall roslaunch
+
+roslaunch turtlebot3_jupyter run.launch & 
+
+sleep 10
+
+rostopic pub /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+pose:
+  pose:
+    position: {x: -3.0, y: 1.0, z: 0.0}
+    orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}
+  covariance: [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.07]"
+
